ca54c50ecbb35f3fe701b247181f03991de79abb
amitaucs <the.amisoft@gmail.com>
2019-11-12 07:47:17 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+   state = {
+       persons: [
+           {name: 'Amit', age: 28},
+           {name: 'Manu', age: 29},
+           {name: 'Bappa', age:26}
+       ]
+   }
+
+   switchNameHandler = () => {
+     //DO NOT DO LIKE THIS :  this.state.persons[0].name='Amit Datta';
+      this.setState({
+          persons: [
+              {name: 'Amit Datta', age: 28},
+              {name: 'Manu', age: 29},
+              {name: 'Bappa', age:27}
+          ] })
+   }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I am a react App</h1>
+        <p>This is really working!!</p>
+
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+
+    );
+
+  }
+}
+
+export default App;

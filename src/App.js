36189cf00af9b02d6cd8904cd12ef83d604fa669
amitaucs <the.amisoft@gmail.com>
2019-11-15 08:53:13 +0530

    method reference, pass by value, event handing to capture test, two way binding

--- src/App.js
+++ src/App.js
@@ -12,14 +12,24 @@ class App extends Component {
        ]
    }
 
-   switchNameHandler = () => {
+   switchNameHandler = (newName) => {
      //DO NOT DO LIKE THIS :  this.state.persons[0].name='Amit Datta';
       this.setState({
           persons: [
-              {name: 'Amit Datta', age: 28},
+              {name: newName, age: 28},
               {name: 'Manu', age: 29},
               {name: 'Bappa', age:27}
           ] })
+   }
+
+
+   nameChangedHandler = (event) =>{
+       this.setState({
+           persons: [
+               {name: 'Amit', age: 28},
+               {name: event.target.value, age: 29},
+               {name: 'Bappa', age:27}
+           ] })
    }
 
   render() {
@@ -28,10 +38,19 @@ class App extends Component {
         <h1>Hi, I am a react App</h1>
         <p>This is really working!!</p>
 
-          <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          <button onClick={()=>this.switchNameHandler('Bappa!!!')}>Switch Name</button>
+          <Person name={this.state.persons[0].name}
+                  age={this.state.persons[0].age}/>
+
+
+          <Person name={this.state.persons[1].name}
+                  age={this.state.persons[1].age}
+                  click={this.switchNameHandler.bind(this,'Bappa')}
+                  changed={this.nameChangedHandler}>
+
+              My hobbies is Racing</Person>
+          <Person name={this.state.persons[2].name}
+                  age={this.state.persons[2].age}/>
       </div>
 
     );
